e6641793f4c29b55925379ee2d5ce38966214447
Riekelt Brands <coderiekelt@users.noreply.github.com>
2020-11-30 02:57:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ chmod 0740 /etc/zabbix/scripts/*
 chown -R zabbix. /etc/zabbix/scripts
 
 echo "Copy config..."
-curl https://raw.githubusercontent.com/leshak/zabbix-mongodb/master/zabbix_agentd.d/mongodb.conf -o /etc/zabbix/zabbix_agentd.d/mongodb.conf
+curl https://raw.githubusercontent.com/leshak/zabbix-mongodb/master/zabbix_agentd.d/mongodb.conf -o /etc/zabbix/zabbix_agentd.conf.d/mongodb.conf
 
 echo "Restart zabbix agent..."
 systemctl restart zabbix-agent
